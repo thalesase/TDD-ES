ba52320cdbdc337fc4230b7474967972d4c2b0fc
thalesase <thales.a.elias@gmail.com>
2020-10-07 14:24:03 -0300

    commit 2

--- Dollar.java
+++ Dollar.java
@@ -1,9 +1,10 @@
 class Dollar {
-    int amount = 10;
 
     Dollar(int amount) {
+        this.amount = amount;
     }
 
     void times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 	    Tests t = new Tests();
 	    t.testMultiplication();
+        t.testEquality();
+	    t.testFrancMultiplication();
     }
 }
